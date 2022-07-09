aa60f2325a823b7f607f3b9549facd30d124c85e
uisproject <uisprojectxyz@gmail.com>
2022-07-09 20:59:10 +0700

    added some contents

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -2,7 +2,23 @@ import MainLayout from "@/layouts/MainLayout";
 import React from "react";
 
 const index = () => {
-  return <MainLayout>This is Content</MainLayout>;
+  return (
+    <MainLayout>
+      <div className="leading-7 max-w-[780px]">
+        <p>
+          A Web Developer that specialized in front-end development using
+          vanilla and various frameworks with some experiences in backend field.
+          Has been passion with tech industry and always learn new things
+          everyday
+        </p>
+        <br />
+        <p>
+          My current focus is to create awesome product using my skill i have
+          learnt so far and becoming better and better😁.
+        </p>
+      </div>
+    </MainLayout>
+  );
 };
 
 export default index;
