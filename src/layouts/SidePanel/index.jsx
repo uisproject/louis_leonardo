ae8222c2b9f2028fd7fd70c61cdb2250ccbcc3c8
uisproject <uisprojectxyz@gmail.com>
2022-07-10 03:02:48 +0700

    display mini avatar when is not intersecting with main avatar

--- src/layouts/SidePanel/index.jsx
+++ src/layouts/SidePanel/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import SidePanelWrapper from "@/components/General/SidePanelWrapper";
 import { useGetUserAPI } from "@/features/getUserAPI";
 import FollowerIcon from "@/icons/FollowerIcon";
+import { useObserverHook } from "@/hooks/useObserverHook";
 
 const SidePanel = () => {
   const { userData, isLoading } = useGetUserAPI();
+  const loadElement = useRef(null);
+  const observe = useObserverHook(loadElement);
 
   return (
     <>
@@ -12,11 +15,11 @@ const SidePanel = () => {
         <SidePanelWrapper>
           <div className="w-[340px]">
             <img
-              className="w-[90%] rounded-full mt-[10px] md:mt-[-25px] relative z-10"
+              className="w-[90%] rounded-full mt-[10px] md:mt-[-25px] md:relative z-10"
               src={`${userData?.avatar_url}`}
               alt="avatar"
             />
-            <div className="text-[#57606a] text-[24px] mb-2">
+            <div ref={loadElement} className="text-[#57606a] text-[24px] mb-2">
               {userData?.login}
             </div>
             <div className="flex">
