f93029d3770df97861e8edb7bc8e677595dc8f6c
uisproject <uisprojectxyz@gmail.com>
2022-07-10 03:02:37 +0700

    display mini avatar when is not intersecting with main avatar

--- src/layouts/Navigation/index.jsx
+++ src/layouts/Navigation/index.jsx
@@ -1,17 +1,36 @@
 import React from "react";
-import SidePanelWrapper from "@/components/General/SidePanelWrapper";
 import OverviewIcon from "@/icons/OverviewIcon";
 import RepoIcon from "@/icons/RepoIcon";
 import CustomLink from "@/components/General/CustomLink";
+import { useGetUserAPI } from "@/features/getUserAPI";
+import { useObserverSelector } from "@/features/useObserver";
 
 const Navigation = () => {
+  const { userData } = useGetUserAPI();
+  const { isVisible } = useObserverSelector();
+
   return (
     <div className="border-b-2 sticky top-0 bg-white">
-      <div className="flex xl:px-[10em] lg:px-[5em]  min-h-[3em] ">
-        <div className="hidden md:block">
-          <SidePanelWrapper />
+      <div className="flex xl:px-[10em] lg:px-[5em] min-h-[3em] ">
+        <div className="hidden md:flex items-center min-w-[350px]">
+          {userData && (
+            <img
+              src={`${userData?.avatar_url}`}
+              className={`w-[30px] h-[30px] rounded-full ${
+                isVisible && "hidden"
+              }`}
+              alt="avatar"
+            />
+          )}
         </div>
-        <div className="flex pl-5">
+        <div className="flex items-center pl-5">
+          <img
+            src={`${userData?.avatar_url}`}
+            className={`w-[30px]  md:hidden h-[30px] mr-3 rounded-full ${
+              isVisible && "hidden"
+            }`}
+            alt="avatar"
+          />
           <CustomLink
             useActive={true}
             className="mr-5"
