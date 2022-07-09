96855f0ab9f5988ff76dc1bb479f310a441836fe
uisproject <uisprojectxyz@gmail.com>
2022-07-10 01:41:54 +0700

    added intro component

---
+++ src/components/Overview/Intro/index.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div className="pt-5">
+      <div className="leading-7 max-w-[780px]">
+        <p>
+          A Web Developer that specialized in front-end development using
+          vanilla and various frameworks with some experiences in backend field.
+          Has been passion with tech industry and always learn new things
+          everyday
+        </p>
+        <br />
+        <p>
+          My current focus is to create awesome product using my skill i have
+          learnt so far and becoming better and better😁.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
