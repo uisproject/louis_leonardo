c62eda1ea3dfd1bc7b42db1d2825a62f97a0c26f
uisproject <uisprojectxyz@gmail.com>
2022-07-09 21:37:46 +0700

    reorder component

--- src/layouts/Navigation/index.jsx
+++ src/layouts/Navigation/index.jsx
@@ -6,20 +6,22 @@ import CustomLink from "@/components/General/CustomLink";
 
 const Navigation = () => {
   return (
-    <div className="flex min-h-[3em] border-b-2">
-      <SidePanelWrapper />
-      <div className="flex">
-        <CustomLink
-          useActive={true}
-          className="mr-5"
-          href="/"
-          icon={<OverviewIcon />}
-        >
-          Overview
-        </CustomLink>
-        <CustomLink useActive={true} href="/repositories" icon={<RepoIcon />}>
-          Repositories
-        </CustomLink>
+    <div className="border-b-2">
+      <div className="flex px-[10em] min-h-[3em] ">
+        <SidePanelWrapper />
+        <div className="flex">
+          <CustomLink
+            useActive={true}
+            className="mr-5"
+            href="/"
+            icon={<OverviewIcon />}
+          >
+            Overview
+          </CustomLink>
+          <CustomLink useActive={true} href="/repositories" icon={<RepoIcon />}>
+            Repositories
+          </CustomLink>
+        </div>
       </div>
     </div>
   );
