77edd58e96ed31bd6125b5af462c9178d1d0f620
uisproject <uisprojectxyz@gmail.com>
2022-07-10 01:36:07 +0700

    change is loading position

--- src/layouts/SidePanel/index.jsx
+++ src/layouts/SidePanel/index.jsx
@@ -7,25 +7,31 @@ const SidePanel = () => {
   const { userData, isLoading } = useGetUserAPI();
 
   return (
-    <SidePanelWrapper>
-      <div className="w-[340px]">
-        <img
-          className="w-[90%] rounded-full mt-[-25px] "
-          src={`${userData?.avatar_url}`}
-          alt="avatar"
-        />
-        <div className="text-[#57606a] text-[24px] mb-2">{userData?.login}</div>
-        <div className="flex">
-          <div className="flex items-center mr-1">
-            <div className="mr-1">
-              <FollowerIcon />
+    <>
+      {!isLoading && (
+        <SidePanelWrapper>
+          <div className="w-[340px]">
+            <img
+              className="w-[90%] rounded-full mt-[-25px] "
+              src={`${userData?.avatar_url}`}
+              alt="avatar"
+            />
+            <div className="text-[#57606a] text-[24px] mb-2">
+              {userData?.login}
             </div>
-            {userData?.followers} follower
+            <div className="flex">
+              <div className="flex items-center mr-1">
+                <div className="mr-1">
+                  <FollowerIcon />
+                </div>
+                {userData?.followers} follower
+              </div>
+              <div>{userData?.following} following</div>
+            </div>
           </div>
-          <div>{userData?.following} following</div>
-        </div>
-      </div>
-    </SidePanelWrapper>
+        </SidePanelWrapper>
+      )}
+    </>
   );
 };
 
