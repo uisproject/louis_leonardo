9f3bc57d32d2b49f972fdcbcc88cd807cd96f2f7
uisproject <uisprojectxyz@gmail.com>
2022-07-10 00:59:58 +0700

    added filter to search repo

--- src/pages/repositories/index.jsx
+++ src/pages/repositories/index.jsx
@@ -3,18 +3,31 @@ import MainLayout from "@/layouts/MainLayout";
 import { useDispatch } from "react-redux";
 import { getUserReposService } from "@/features/getUserReposAPI";
 import { useGetUserReposAPI } from "@/features/getUserReposAPI";
+import RepoItem from "@/components/Repositories/RepoItem";
+import FilterRepo from "@/components/Repositories/FilterRepo";
 
 const RepositoriesPage = () => {
   const dispatch = useDispatch();
-  const data = useGetUserReposAPI();
-
-  console.log(data);
+  const { filteredRepo, isLoading } = useGetUserReposAPI();
 
   useEffect(() => {
     dispatch(getUserReposService());
   }, []);
 
-  return <MainLayout>Repositories</MainLayout>;
+  return (
+    <MainLayout>
+      <div className="mb-5">
+        <FilterRepo />
+        <hr className="border-[#bcbcbc]" />
+        {isLoading
+          ? "Is loading"
+          : filteredRepo.map((repo, index) => (
+              <RepoItem key={index} {...repo} />
+            ))}
+        <hr className="border-[#bcbcbc]" />
+      </div>
+    </MainLayout>
+  );
 };
 
 export default RepositoriesPage;
