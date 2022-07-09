07e8dec55ba2f8efba79f98dff53ee35bd9f4481
uisproject <uisprojectxyz@gmail.com>
2022-07-10 03:00:57 +0700

    added observer into store

--- src/features/store.js
+++ src/features/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import getUserAPIReducer from "./getUserAPI";
 import getUserReposAPIReducer from "./getUserReposAPI";
+import useObserverReducer from "./useObserver";
 
 const store = configureStore({
   reducer: {
     getUserAPI: getUserAPIReducer,
     getUserRepos: getUserReposAPIReducer,
+    useObserver: useObserverReducer,
   },
 });
 
