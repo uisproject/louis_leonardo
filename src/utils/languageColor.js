a5efe580a648a88aad6c3e5fcffdd6bf041b2115
uisproject <uisprojectxyz@gmail.com>
2022-07-09 21:58:40 +0700

    added utils for utilization tool

---
+++ src/utils/languageColor.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    name: "JavaScript",
+    color: "#f1e05a",
+  },
+  {
+    name: "TypeScript",
+    color: "#2b7489",
+  },
+  {
+    name: "PHP",
+    color: "#6e5494",
+  },
+  {
+    name: "Svelte",
+    color: "#5a6168",
+  },
+  {
+    name: "HTML",
+    color: "#e34f26",
+  },
+];
