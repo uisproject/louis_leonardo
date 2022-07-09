457c631b65cea5c9ceead9feef1d58914518fc8c
uisproject <uisprojectxyz@gmail.com>
2022-07-10 01:23:32 +0700

    added debounce when search

--- src/components/Repositories/FilterRepo/index.jsx
+++ src/components/Repositories/FilterRepo/index.jsx
@@ -3,6 +3,7 @@ import CustomButton from "@/components/General/CustomButton";
 import CustomInput from "@/components/General/CustomInput";
 import { setSearch } from "@/features/getUserReposAPI";
 import { useDispatch } from "react-redux";
+import debounce from "@/utils/debounce";
 
 const FilterRepo = () => {
   const dispatch = useDispatch();
@@ -11,9 +12,11 @@ const FilterRepo = () => {
     <div className="my-5 flex items-center ">
       <div className="mr-2 w-[100%]">
         <CustomInput
-          onChange={(e) => {
-            dispatch(setSearch(e.target.value));
-          }}
+          onChange={(e) =>
+            debounce(() => {
+              dispatch(setSearch(e.target.value));
+            }, 500)
+          }
           placeholder="Find a repository"
         />
       </div>
