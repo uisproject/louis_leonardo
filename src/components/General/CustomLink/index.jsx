12284b2e4802c902309f748b2df4efa50e08669f
uisproject <uisprojectxyz@gmail.com>
2022-07-09 19:25:51 +0700

    adding more conditional for future use

--- src/components/General/CustomLink/index.jsx
+++ src/components/General/CustomLink/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
-const CustomLink = ({ children, icon, className, href = "#" }) => {
+const CustomLink = ({
+  children,
+  icon,
+  className,
+  useActive = false,
+  href = "#",
+}) => {
+  const router = useRouter();
+  console.log(router.asPath);
+  console.log(href);
   return (
     <Link href={href} passHref={true}>
-      <a className={`flex items-center ${className}`}>
+      <a
+        className={`flex items-center h-100 ${className || ""}${" "}${
+          useActive && router.asPath === href
+            ? "border-b-4 border-[#FD8C73]"
+            : ""
+        }`}
+      >
         {icon && <div className="pr-2">{icon}</div>}
         {children}
       </a>
