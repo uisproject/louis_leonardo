d3447bccaf56ca74b15d3db34b75e415648dba2f
uisproject <uisprojectxyz@gmail.com>
2022-07-09 19:26:08 +0700

    modify customlink property

--- src/layouts/Navigation/index.jsx
+++ src/layouts/Navigation/index.jsx
@@ -1,10 +1,26 @@
 import React from "react";
 import SidePanelWrapper from "@/components/General/SidePanelWrapper";
+import OverviewIcon from "@/icons/OverviewIcon";
+import RepoIcon from "@/icons/RepoIcon";
+import CustomLink from "@/components/General/CustomLink";
+
 const Navigation = () => {
   return (
-    <div className="flex">
+    <div className="flex min-h-[3em] border-b-2">
       <SidePanelWrapper />
-      <div>This is navigation</div>
+      <div className="flex flex-wrap">
+        <CustomLink
+          useActive={true}
+          className="mr-5"
+          href="/"
+          icon={<OverviewIcon />}
+        >
+          Overview
+        </CustomLink>
+        <CustomLink useActive={true} href="/repositories" icon={<RepoIcon />}>
+          Repositories
+        </CustomLink>
+      </div>
     </div>
   );
 };
