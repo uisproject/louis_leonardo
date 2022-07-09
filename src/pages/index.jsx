4448787440c7bdba2463aa4a8f043290efc9c26a
uisproject <uisprojectxyz@gmail.com>
2022-07-10 01:42:18 +0700

    split component into smaller component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,11 @@
-import MainLayout from "@/layouts/MainLayout";
 import React from "react";
+import MainLayout from "@/layouts/MainLayout";
+import Intro from "@/components/Overview/Intro";
 
 const index = () => {
   return (
     <MainLayout>
-      <div className="leading-7 max-w-[780px]">
-        <p>
-          A Web Developer that specialized in front-end development using
-          vanilla and various frameworks with some experiences in backend field.
-          Has been passion with tech industry and always learn new things
-          everyday
-        </p>
-        <br />
-        <p>
-          My current focus is to create awesome product using my skill i have
-          learnt so far and becoming better and better😁.
-        </p>
-      </div>
+      <Intro />
     </MainLayout>
   );
 };
